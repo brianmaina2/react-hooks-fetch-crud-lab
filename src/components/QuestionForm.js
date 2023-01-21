989faa39881849d4c8f23a96b8df9ad233a3b34c
brian.maina@student.moringaschool.com <brianmaina2>
2023-01-21 12:01:48 +0300

    Done

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 
 function QuestionForm(props) {
+
+
   const [formData, setFormData] = useState({
     prompt: "",
     answer1: "",
@@ -9,7 +11,6 @@ function QuestionForm(props) {
     answer4: "",
     correctIndex: 0,
   });
-
   function handleChange(event) {
     setFormData({
       ...formData,
@@ -17,14 +18,39 @@ function QuestionForm(props) {
     });
   }
 
+
+
   function handleSubmit(event) {
     event.preventDefault();
     console.log(formData);
+
+    //create the question data to be posted
+    const questionData = {
+      prompt: formData.prompt,
+      answers: [
+        formData.answer1,
+        formData.answer2,
+        formData.answer3,
+        formData.answer4
+
+      ],
+      correctIndex: parseInt(formData.correctIndex),
+    }
+
+
+    fetch("http://localhost:4000/questions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(questionData)
+    });
   }
 
   return (
     <section>
       <h1>New Question</h1>
+      <h1>Add New Question</h1>
       <form onSubmit={handleSubmit}>
         <label>
           Prompt:
@@ -89,5 +115,4 @@ function QuestionForm(props) {
     </section>
   );
 }
-
 export default QuestionForm;
